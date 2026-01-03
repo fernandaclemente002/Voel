487fe678a82f55a1d6021e97a1d2d491c4912eae
Fernanda Clemente <fernandaclemente002@users.noreply.github.com>
2026-01-03 00:52:48 -0300

    vai plmds

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import ResetPassword from './pages/ResetPassword'
+import { Navigate } from 'react-router-dom';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-export default App
+<Routes>
+  <Route path="/" element={<Navigate to="/login" />} />
+  <Route path="/login" element={<Login />} />
+  <Route path="/register" element={<Register />} />
+  <Route path="/reset-password" element={<ResetPassword />} />
+</Routes>
